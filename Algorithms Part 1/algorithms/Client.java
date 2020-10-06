78435a43eacc8da47bfc9893e59e93feaa1b5de9
pshivam111 <pshivam111@gmail.com>
2020-10-06 23:00:05 +0530

    plus plus

--- Algorithms Part 1/algorithms/Client.java
+++ Algorithms Part 1/algorithms/Client.java
@@ -3,7 +3,6 @@ package algorithms;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-import algorithms.UnionFind;
 class Client {
 
     public static void main(String[] args) {
@@ -16,7 +15,7 @@ class Client {
 
             UnionFind uf=new UnionFind(N);
 
-            
+            uf.union(3, 4);
             scan.close();
         } catch (FileNotFoundException e) {
             System.out.println("File not found...");
